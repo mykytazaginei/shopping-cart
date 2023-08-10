a8b3c79f9ffe06ceb623b177a433908f11cfcebc
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-08-10 21:27:50 +0200

    fix

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems'
 
 const initialState = {
-  cartItem: [],
+  cartItem: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
