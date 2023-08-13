a3685cf358593fbc4dff327608bff7248da816c5
Nikita Zaginey <nikitazaginey1@gmail.com>
2023-08-13 19:23:57 +0200

    implement async in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import NavBar from "./components/NavBar";
 import CartContainer from "./components/cartContainer";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateeTotals } from "./features/cart/cartSlice";
+import { calculateeTotals, getCartItmes } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   useEffect(() => {
     dispatch(calculateeTotals());
   }, [cartItems]);
 
   const { isOpen } = useSelector((store) => store.modal);
+
+  useEffect(() => {
+    dispatch(getCartItmes());
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h4>Loading...</h4>
+      </div>
+    );
+  }
 
   return (
     <main>
